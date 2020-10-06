0e6d41f455785011819415ddaf7e85f12999ca00
Ajay Yamkar <58873896+Ajyamkar@users.noreply.github.com>
2020-10-06 17:25:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var count=0;
+$(".navbar-button").click(function(){
+     $("#navbarTogglerDemo02").toggleClass("clickNavbar");
+     $("#close-iconId").toggleClass("close-icon");
+    $("#toggler-icon").toggleClass("navbar-toggler-icon");
+})
